36cb91442fe3418307123f9e6c8641d0789ece4a
hakoningir <hir12@hi.is>
2023-08-30 12:42:45 +0000

    dæmi 4 komið

--- RandTriangle/randtriangle.js
+++ RandTriangle/randtriangle.js
@@ -14,7 +14,17 @@ window.onload = function init()
                       vec2(0.1, 0),
                       vec2(0, 0.1));
     }
-    
+    for (let i = 0; i < vertices.length; i += 3){
+        var x = -1+Math.random()*2;
+        var y = -1+Math.random()*2;
+        vertices[i][0]+=x; 
+        vertices[i][1]+=y; 
+        vertices[i+1][0]+=x; 
+        vertices[i+1][1]+=y; 
+        vertices[i+2][0]+=x; 
+        vertices[i+2][1]+=y; 
+        //færa alla punkta um randomtölu á bilinu 0 og 1
+    }
     // var u = add( vertices[0], vertices[1]);
     // var v = add( vertices[0], vertices[2]);
     // var t = scale(Math.random(), add(u,v));
@@ -47,27 +57,18 @@ window.onload = function init()
     // Find the location of the variables colors and offset in the shader program
     colorLoc = gl.getUniformLocation( program, "colors" );
     offsetLoc = gl.getUniformLocation( program, "offset" );
-    console.log(vertices);
     render();
 };
 
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
-    for (let i = 0; i <= vertices.length; i += 3){
-        var x = Math.random();
-        var y = Math.random();
-        vertices[i.x]+=x; 
-        vertices[i.y]+=y; 
-        vertices[(i+1).x]+=x; 
-        vertices[(i+1).y]+=y; 
-        vertices[(i+2).x]+=x; 
-        vertices[(i+2).y]+=y; 
-        //færa alla punkta um randomtölu á bilinu 0 og 1
-    }
+
     for(let i = 0; i < 100; i+=3){
+        console.log(vertices);
         gl.uniform4fv( colorLoc, vec4(Math.random(), Math.random(), Math.random(), 1) );
+        
         gl.uniform2fv( offsetLoc, vertices );
-        gl.drawArrays( gl.TRIANGLES, 0, vertices.length );   
+        gl.drawArrays( gl.TRIANGLES, i, 3 );   
     }
 }
