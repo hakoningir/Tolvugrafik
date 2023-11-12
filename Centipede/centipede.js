575929bec24182a25f692c6a7ccc04a7fcfdaabd
hakoningir <hir12@hi.is>
2023-11-12 23:36:17 +0000

    sveppir og centipede svona næstum þvi

--- Centipede/centipede.js
+++ Centipede/centipede.js
@@ -1,7 +1,11 @@
-const canvas = document.querySelector('#c');
-
+const canvas = document.querySelector("#c");
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  canvas.clientWidth / canvas.clientHeight,
+  0.1,
+  1000
+);
 camera.position.set(0, 0, 10);
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
@@ -13,52 +17,90 @@ const playerGeometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
 const shotMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const player = new THREE.Mesh(playerGeometry, material);
-player.position.y = -1 * (window.innerHeight / 2) / 100;
+player.position.y = -5;
 player.position.z = -5;
 scene.add(player);
 
 const shotArray = [];
+const mushroomArray = [];
 
-
+function createMushroom(x, z) {
+    const mushroomGeometry = new THREE.SphereGeometry(0.3, 32, 16);
+    const mushroomMaterial = new THREE.MeshBasicMaterial({
+        color: 0x00ff00,
+    });
+    const mushroom = new THREE.Mesh(mushroomGeometry, mushroomMaterial);
+  
+    mushroom.position.x = x;
+    mushroom.position.y = -4.8;
+    mushroom.position.z = z;
+  
+    scene.add(mushroom);
+    mushroomArray.push(mushroom);
+}
+  
+function initMushrooms() {
+    for (let i = 0; i < 14; i++) {
+        const randX = Math.random() * 10 - 5;
+        const randZ = Math.random() * 10 - 5;
+        createMushroom(randX, randZ);
+    }
+}
+initMushrooms();  
 
 function shoot() {
     const shotGeometry = new THREE.BoxGeometry(0.1, 0.5, 0.1);
     const shot = new THREE.Mesh(shotGeometry, shotMaterial);
-
-    // Set initial position of the shot in the xy-plane and away from the player
+  
     shot.position.x = player.position.x;
     shot.position.y = player.position.y + 0.2;
-    shot.position.z = player.position.z + 2; // Adjust th e initial distance away from the player
-
-    // Set the rotation of the shot to be horizontal
-    shot.rotation.set(Math.PI/2, 0, 0 );
-
-    shotArray.push(shot);
+    shot.position.z = player.position.z + 2;
+  
+    shot.rotation.set(Math.PI / 2, 0, 0);
+  
     scene.add(shot);
+    shotArray.push(shot);
 }
-
-
+  
 function shotMovement() {
-    shotArray.forEach(shot => {
-        const speed = 0.2;
-
-        // Move the shot straight ahead in the positive z-axis direction
-        shot.position.z += speed;
+    const speed = 0.2;
+  
+    shotArray.forEach((shot) => {
+      shot.position.z += speed;
     });
 }
 
-function collisionDetection(){
-    shotArray.forEach(shots => {
-        allTeemoShrooms.forEach(teemo => {
-            if( shots.position.z - 0.1 <= teemo.position.z + 0.1 &&
-                shots.position.z + 0.1 >= teemo.position.z - 0.1 &&
-                shots.position.x - 0.1 <= teemo.position.x + 0.1 &&
-                shots.position.x + 0.1 >= teemo.position.x - 0.1 ){
-                scene.remove(shots)
-                return true;
+function collisionDetection() {
+    shotArray.forEach((shot) => {
+        mushroomArray.forEach((mushroom) => {
+            const shotRadius = 0.1;
+            const mushroomRadius = 0.3;
+  
+            const dx = shot.position.x - mushroom.position.x;
+            const dz = shot.position.z - mushroom.position.z;
+            const distanceSquared = dx * dx + dz * dz;
+  
+            if (distanceSquared < (shotRadius + mushroomRadius) ** 2) {
+                scene.remove(shot);
+                scene.remove(mushroom);
+  
+                const shotIndex = shotArray.indexOf(shot);
+                const mushroomIndex = mushroomArray.indexOf(mushroom);
+                
+                if (shotIndex !== -1) shotArray.splice(shotIndex, 1);
+                if (mushroomIndex !== -1) mushroomArray.splice(mushroomIndex, 1);
+  
+                respawnMushroom();
             }
         });
     });
+}
+
+function respawnMushroom() {
+    const randX = Math.random() * 10 - 5;
+    const randZ = Math.random() * 10 - 5;
+  
+    createMushroom(randX, randZ);
 }
 
 let centiArray = []
@@ -76,9 +118,20 @@ function centipede(){
 }
 centipede();
 
-function moveCentipede(){
+function moveCentipede() {
+    let dir = 1;
     centiArray.forEach(centiElement => {
-        centiElement.x += 0.2;
+        const speed = 0.05;
+        centiElement.position.x += speed * dir;
+
+        if (centiElement.position.x >= 8 ) {
+            centiElement.position.x -= 0.05;
+            dir *= -1;
+        }
+        if(centiElement.position.x <= -8){
+            centiElement.position.x += 0.05;
+            dir *= -1;
+        }
     });
 }
 
@@ -88,13 +141,13 @@ function keycodes(e){
             player.position.z += 0.2;
             break;
         case "ArrowLeft":  
-            player.position.x -= 0.2;
+            player.position.x += 0.2;
             break;
         case "ArrowDown":	
             player.position.z -= 0.2;
             break;
         case "ArrowRight":
-            player.position.x += 0.2;
+            player.position.x -= 0.2;
             break;
         case "KeyW":	
             player.position.z += 0.2;
@@ -129,11 +182,10 @@ function animate() {
     
     camera.lookAt(player.position );
     moveCentipede();
+    collisionDetection();
     shotMovement();
     renderer.render(scene, camera);
 }
 
-animate();
-teemoShrooms()
-
 document.addEventListener("keydown", keycodes, false);
+animate();
